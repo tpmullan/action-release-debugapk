f895d3800c7ecbe21e50941c5425b01d2a8610c9
Tom Mullaney <2078150+tpmullan@users.noreply.github.com>
2023-02-09 00:31:39 -0600

    Fixed bash error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 
-hub checkout ${${REPO_BRANCH}:-master}
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+hub checkout ${REPO_BRANCH:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
